67f55fbbc4793f3492b6cc59683cb3a9a8de4e07
Paul Crowder <paul.crowder@blackbaud.com>
2016-09-19 09:56:08 -0400

    Moved items to vendor

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,23 +1,13 @@
-// For vendors for example jQuery, Lodash, angular2-jwt just import them here unless you plan on
-// chunking vendors files for async loading. You would need to import the async loaded vendors
-// at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
-// run `typings install x` where `x` is your module
-
-// Angular 2
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
+import '@angular/forms';
 
-// RxJS
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/observable/PromiseObservable';
+import 'rxjs/observable/fromPromise.js';
+import 'rxjs/operator/toPromise.js';
 
-// Dragula
 import 'ng2-dragula/ng2-dragula';
-
-// if ('production' === ENV) {
-  // Production
-// } else {
-  // Development
-// }
